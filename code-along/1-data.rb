2198989ada69f8fd7e3ce8f60558d64263f1c3a7
Austin Tang <austin.tang@kellogg.northwestern.edu>
2025-01-28 20:29:23 +0000

    in class learning

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,59 @@
 
 # Numbers
 
-# Non-numbers
+# puts 2
+# puts 5
 
-# True, False
+# puts 5 + 2
+# puts 5 * 2
+# puts 5 - 2
+# puts 5 / 2 #this outputs 2 because we're using integers and it will not include the remainder
+# puts 5 / 2.0 #this will output with the float data type and show 2.5
 
-# Nothing
+# puts (2 + 5) * 5
+
+# # Non-numbers
+
+# puts "Hello, world!"
+
+# puts "Tacos are" + " delish"
+
+# puts "tacos" * 3
+
+# puts "tacos:" + 3.to_s #needs to convert the 3 to string otherwise it's going to have an error
+
+
+# # True, False
+
+# puts true
+# puts false
+
+
+# # Nothing
+
+# puts nil
 
 # Variables
 
-# Combine strings and variables
+# food = "tacos"
+# quantity = 3
+
+# puts food * quantity
+
+# # Combine strings and variables
+
+# puts "tacos: #{quantity}"
+
+# puts "#{food}: #{quantity}"
 
 # String manipulation
+
+puts "Hello".length #outputs the length of teh string "Hello"
+puts "how are you today?".capitalize #adds a capitalization to the start of the string
+puts "computer".reverse #reverses the string
+
+creed = "This Is The Way"
+
+puts creed.upcase #makes everything uppercase
+puts creed.downcase #makes everything lowercase
+puts creed.swapcase #inverts the case of each letter
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,14 +2,69 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
-# Booleans
+# # Booleans
 
-# Boolean Expressions
+# test_is_true = true
+# test_is_false = false
+# puts test_is_false
 
-# If Conditional Logic
+# # Boolean Expressions
 
-# If/Else Conditional Logic
+# puts 3 == 2
+# puts 3 != 2
+# puts 3 > 2
+# puts 3 < 2
 
-# Elsif Conditional Logic
+# # If Conditional Logic
+
+# if 3 == 2
+#     puts "This text should never display."
+# end
+
+# if 3 > 2 
+#     puts "This text should always display."
+# end
+
+
+
+# # If/Else Conditional Logic
+
+# if 3 == 2
+#     puts "This text should never display."
+# else
+#     puts "This text should always display."
+# end
+
+# user_entered_password = "tacos"
+# real_password = "secret"
+
+# if user_entered_password == real_password
+#     puts "Your password matches. Welcome back!"
+# else
+#     puts "Wrong password. Try again."
+# end
+
+# # Elsif Conditional Logic
+
+# your_team_score = 2
+# other_team_score = 2
+
+# if your_team_score > other_team_score
+#     puts "You won!"
+# elsif your_team_score == other_team_score
+#     puts "Tied"
+# else
+#     puts "Your team lost"
+# end
 
 # Combining Expressions
+
+temp = 68
+precipitation = 0
+
+#pseudo code... if temp >= 65 and temp <= 75 and precipitation == 0 then put "It's perfect outside."
+
+if temp >= 65 && temp <= 75 && precipitation == 0 #using || indicates or
+    puts "It's perfect outside."
+end
+
